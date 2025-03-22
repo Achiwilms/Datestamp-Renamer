0e3cf84bc6268e5385a3507fb54f6a2f44faabdd
Achiwilms <philip901203777@gmail.com>
2025-03-22 10:51:47 +0800

    update

--- datestamp_renamer.py
+++ datestamp_renamer.py
@@ -3,27 +3,30 @@ from utils.get_date import get_date
 from utils.rename_file import rename_file
 
 def rename_files_by_date(file_dir):
-    for filename in os.listdir(file_dir):
-        # find extension
-        _, extension = os.path.splitext(filename)
+    for subdir_name in os.listdir(file_dir):
+        subdir_path = os.path.join(file_dir, subdir_name)
+        for filename in os.listdir(subdir_path):
+            # find extension
+            _, extension = os.path.splitext(filename)
 
-        # construct filepath
-        filepath = os.path.join(file_dir, filename)
-        
-        # get creation date
-        file_date = get_date(filepath)
+            # construct filepath
+            filepath = os.path.join(subdir_path, filename)
+            
+            # get creation date
+            file_date = get_date(filepath)
 
-        # construct new filepath
-        new_path = os.path.join(file_dir, file_date+extension)
+            # construct new filepath
+            new_path = os.path.join(subdir_path, file_date+extension)
 
-        # rename file
-        rename_file(filepath, new_path)
+            # rename file
+            rename_file(filepath, new_path)
+    
     return   
 
 if __name__ == "__main__":
     # read arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_dir', type=str, default='12-23', help='file directory to be renamed by date')
+    parser.add_argument('--file_dir', type=str, default='/media/veracrypt2/Journal/2023', help='file directory to be renamed by date')
     args = parser.parse_args()
     # rename files
     rename_files_by_date(args.file_dir)
